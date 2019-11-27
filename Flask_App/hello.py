08487a315275ffb1d799125d31f7c128a105590e
earu_devops <aru3184@gmail.com>
2019-11-27 15:53:36 +0530

    Add hello.py to remote repo

--- Flask_App/hello.py
+++ Flask_App/hello.py
@@ -2,7 +2,7 @@
 from flask import Flask
 
 app = Flask(__name__)  #creating the Flask class Object
-@app.route('/')  #route decorator defines the URL
+@app.route('/')  #route decorator defines the URL for mapping of the associated function
 
 #Create Function
 def home():
